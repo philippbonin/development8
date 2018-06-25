9fece0c4a3f35c94f1eee27b0d3086741bd96eb8
Philipp Bonin <p.bonin@telekom.de>
2018-06-25 11:14:33 +0200

    add google calendar

--- js/development8.js
+++ js/development8.js
@@ -1,5 +1,53 @@
 $( document ).ready(function() {
-  
+
+  // Client ID and API key from the Developer Console
+      var CLIENT_ID = '545719211521-fn3vbeh14don8lqbvj5osaj78b24851r.apps.googleusercontent.com';
+      var API_KEY = 'AIzaSyCeP3J-NySJre0p4VOjibCL_4xNvpvjibE';
+
+      // Array of API discovery doc URLs for APIs used by the quickstart
+      var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+
+      // Authorization scopes required by the API; multiple scopes can be
+      // included, separated by spaces.
+      var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+
+      /**
+       *  On load, called to load the auth2 library and API client library.
+       */
+      function handleClientLoad() {
+        gapi.load('client:auth2', initClient);
+      }
+
+      function initClient() {
+        gapi.client.init({
+          apiKey: API_KEY,
+          clientId: CLIENT_ID,
+          discoveryDocs: DISCOVERY_DOCS,
+          scope: SCOPES
+        }).then(function () {
+          execute()
+        });
+      }
+
+  var d = new Date();
+  var currentDay =  d.getFullYear() +'-'+ (d.getMonth() + 1) +'-'+ d.getDate();
+  var currentDev8Day;
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute(timeMax, timeMin) {
+    return gapi.client.calendar.events.list({
+      "calendarId": "e6tmn99nd2d7ts9vuvv7virf04@group.calendar.google.com",
+      "alwaysIncludeEmail": "false",
+      "timeMax": currentDay+"T23:59:00Z",
+      "timeMin": currentDay+"T00:00:01Z",
+    }).then(function(response) {
+                currentDev8Day = response.result.items[0].summary;
+                setOnOff(dev8.indexOf(currentDev8Day));
+              },
+              function(err) { console.error("Execute error", err); 
+    });
+  }
+
+
   var FirstSection = $('#day-01-07');
   var FirstSectionFile = 'description/day-01-07.html';
   var dev8Parts = $('svg .day, svg .day-group').children();
@@ -45,6 +93,8 @@ $( document ).ready(function() {
       setNav($('.nav-question'));
       
       var type = selector.data("type");
+
+      id = (id == undefined) ? 'default' : id;
       var file = 'description/' + id + '.html';
 
       loadFile(file, type);
@@ -78,6 +128,8 @@ $( document ).ready(function() {
       i++;                     
       if (i < dev8.length) {            
          blingbling();             
+      }else{
+        handleClientLoad();
       }                   
     }, 300)
   }
